685758a765f771807742c89d5f283dbdfc46261b
= <=>
2014-01-24 16:24:06 -0700

    Added user controller and new user signup page with rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject {page}
+
   describe "Home page" do
     before {visit root_path}
-    subject {page}
-
     it {should have_content('Sample App')}
     it {should have_title(full_title(''))}
     it {should_not have_title(' | Home')}
@@ -13,21 +13,18 @@ describe "Static pages" do
 
   describe "Help page" do
     before {visit help_path}
-    subject {page}
     it{should have_content 'Help'}
     it{should have_title full_title('Help')}
   end
 
   describe "About page" do
     before {visit about_path}
-    subject {page}
     it{should have_content('About Us')}
     it{should have_title(full_title('About Us'))}
   end
 
   describe "Contact page" do
     before {visit contact_path}
-    subject {page}
     it{should have_content('Contact')}
     it{should have_title(full_title('Contact'))}
   end
