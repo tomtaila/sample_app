6b0e58d2dee1de41c0d17acfcf2eb6b6650af713
= <=>
2014-01-27 13:10:38 -0700

    Finish user sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+  	#require(key) Ensures that a parameter is present. If it's present, returns the parameter at the given key, otherwise raises an ActionController::ParameterMissing error.
+  	#permit(key[]) Sets the permitted attribute to true. This can be used to pass mass assignment. Returns self.
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl .define do
+	factory :user do 
+		name "Tom Taila"
+		email "tomtaila@hotmail.com"
+		password "password"
+		password_confirmation "password"
+	end
+end
