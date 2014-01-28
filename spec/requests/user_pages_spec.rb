fb64c06a730bc5dcc4f33a38c6eb096791669482
= <=>
2014-01-27 21:56:18 -0700

    Sign in user immediately after sign up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,7 +24,6 @@ describe 'User pages' do
 	    describe "with invalid information" do
 	      it "should not create a user" do
 	        expect{click_button submit}.not_to change(User, :count)
-	        #EXPECT ERROR MESSAGES TO BE IN CONTENT
 	      end
 	    end
 
@@ -38,6 +37,17 @@ describe 'User pages' do
 
 	      it "should create a user" do
 	        expect{click_button submit}.to change(User, :count).by(1)
+	      end
+
+	      describe "after saving the user" do
+	      	before{click_button submit}
+	      	let(:user) {User.find_by(email: 'user@example.com')}
+
+	      	it{should have_link('Sign out', href: signout_path)}
+	      	it{should have_link('Profile', href: user_path(user))}
+	      	it{should_not have_link('Sign in', href: signin_path)}
+	      	it{should have_title(user.name)}
+	      	it{should have_selector('div.alert.alert-success', text: 'Welcome')}
 	      end
 	    end
 	end
