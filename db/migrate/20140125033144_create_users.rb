4e5fa266498c2578599fb13eb27feb372c9e55a5
= <=>
2014-01-28 09:51:06 -0700

    Test fix for heroku

--- db/migrate/20140125033144_create_users.rb
+++ db/migrate/20140125033144_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name #create name column in database
       t.string :email #create email column in database
-      t.string :password_digest #For hash function of password
+      #t.string :password_digest #For hash function of password
 
       t.timestamps #create 'created_at' and 'updated_at' columns in database
     end
